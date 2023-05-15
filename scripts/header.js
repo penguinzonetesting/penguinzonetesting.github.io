faa8e13c9836e68844fa7426373e4b29455cc4ef
Blackout03 <gardner.hayden03@gmail.com>
2023-05-15 15:31:27 +1000

    Quick Test

--- scripts/header.js
+++ scripts/header.js
@@ -1,20 +1,20 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
-	function scrollText(lines) {
-		const scrollingText = document.getElementById("scrolling-text");
+	function rotateText(lines) {
+		const rotatingText = document.getElementById("rotating-text");
 
 		if (lines.length === 1) {
-			scrollingText.textContent = lines[0];
-			scrollingText.style.opacity = "1";
-			scrollingText.style.animationIterationCount = "0";
+			rotatingText.textContent = lines[0];
+			rotatingText.style.opacity = "1";
+			rotatingText.style.animationIterationCount = "0";
 			return; // Exit the function since there's only one line
 		}
 
 		let index = 0;
 
-		scrollingText.addEventListener("animationiteration", updateText);
+		rotatingText.addEventListener("animationiteration", updateText);
 
 		function updateText() {
-			scrollingText.textContent = lines[index];
+			rotatingText.textContent = lines[index];
 			index = (index + 1) % lines.length; // Increment index and loop back to 0 when reaching the last line
 		}
 
@@ -27,7 +27,7 @@
 		.then((response) => response.json())
 		.then((data) => {
 			const lines = data.lines;
-			scrollText(lines);
+			rotateText(lines);
 		})
 		.catch((error) => {
 			console.error("Error fetching JSON data:", error);
