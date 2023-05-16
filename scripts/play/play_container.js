bfae28c2aa4773f5b1321910419ba504df94086e
Blackout03 <gardner.hayden03@gmail.com>
2023-05-17 03:03:07 +1000

    Wake Up Babe! New Website Update Just Dropped!
     - Events page exists
     - Play page now checks if you have flash and shows an image if you do not
     - Small Screen, Medium Screen and Large Screen buttons exist but do nothing atm

---
+++ scripts/play/play_container.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+	const container = document.getElementById('play_container');
+
+	// Apply CSS styles to trim the container
+	container.style.overflow = 'hidden'; // Hide any content outside the container's bounds
+	container.style.borderRadius = '10px'; // Set the border radius
+});
