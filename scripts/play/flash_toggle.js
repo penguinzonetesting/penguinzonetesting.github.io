ea05a1f00938e558a090620e4ac9b82813d2e6fb
Blackout03 <gardner.hayden03@gmail.com>
2023-05-18 22:48:42 +1000

    Small Medium and Large Screen buttons now work!
    You can now play at different screen sizes to suit your leisure.
    
    Medium Screen Size will be the only one that changes based on screen size.

---
+++ scripts/play/flash_toggle.js
@@ -0,0 +1,37 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+	function isElectron() {
+		// Renderer process
+		if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+			return true;
+		}
+
+		// Main process
+		if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+			return true;
+		}
+
+		// Detect the user agent when the `nodeIntegration` option is set to true
+		if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	const container = document.getElementById('play_container');
+	const no_flash = document.getElementById('noflash');
+	const left_navbar = document.getElementById('left-navbar');
+	const right_navbar = document.getElementById('right-navbar');
+
+	if (isElectron() || swfobject.hasFlashPlayerVersion("1")) {
+		container.style.display = "flex";
+		no_flash.style.display = "nome";
+		left_navbar.style.paddingRight = "0";
+		right_navbar.style.display = "flex";
+	} else {
+		container.style.display = "none";
+		no_flash.style.display = "flex"
+		left_navbar.style.paddingRight = "20.5rem";
+		right_navbar.style.display = "none";
+	}
+});
