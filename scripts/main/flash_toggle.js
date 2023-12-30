eb069cffe2785d1a7726c86a0eefe979b7f7e87b
Blackout03 <gardner.hayden03@gmail.com>
2023-12-30 14:19:40 +1100

    hmm

---
+++ scripts/main/flash_toggle.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+	function isElectron() {
+		return (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer')
+			|| (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron)
+			|| (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0);
+	}
+
+
+	const scene_container = document.getElementById('scene_container');
+	const no_flash = document.getElementById('noflash');
+
+	if (isElectron() || swfobject.hasFlashPlayerVersion("1")) {
+		console.log("Electron Found");
+		scene_container.style.display = "";
+		no_flash.style.display = "";
+	} else {
+		console.log("Electron Not Found");
+		scene_container.style.display = "none";
+		no_flash.style.display = "flex"
+	}
+});
--- scripts/main/scenes.js
+++ scripts/main/scenes.js
@@ -1,16 +1,16 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
 	const scenes = [
 		"scene-default.swf",
-		"scene-halloween.swf",
-		"scene-penguingames.swf",
-		"scene-holiday.swf",
 		// Add more scene file names as needed
 	];
 
 	const randomIndex = Math.floor(Math.random() * scenes.length);
 	const randomScene = scenes[randomIndex];
+	console.log(scenes.length)
+	console.log(randomIndex)
+	console.log(randomScene)
 
-	document.getElementById("scene").innerHTML = `
+	document.getElementById("scene_container").innerHTML = `
 			<object width="100%" height="100%" data="../home_scenes/${randomScene}">
 				<param name="movie" value="../home_scenes/${randomScene}">
 				<param name="quality" value="high">
